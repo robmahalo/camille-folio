46c5f67cb08ea0c32e55f8b70681bcfa4d57db26
Robby Manalo <44278625+robmahalo@users.noreply.github.com>
2024-03-26 17:43:55 -0500

    chore: fix formatting

--- frontend/src/components/home-card-list/homecardlist.component.jsx
+++ frontend/src/components/home-card-list/homecardlist.component.jsx
@@ -43,36 +43,37 @@ const HomeCardList = () => {
         </div>
         <div data-aos="fade-up" data-aos-duration="1000">
           <div className="card-list-alt">
-            <div className="card-black"></div>
-            <div>
+            <div className="flex-container">
               <div className="card-black">
-                <img
-                  className="image-container"
-                  src={zara}
-                  alt="zara-project"
-                ></img>
+                <div>
+                  <img
+                    className="image-container"
+                    src={zara}
+                    alt="zara-project"
+                  ></img>
+                </div>
+                <h2>Zara Redesign</h2>
               </div>
-              <h2>Zara Redesign</h2>
-            </div>
-            <div>
-              <div className="card-black">
-                <img
-                  className="image-container"
-                  src={webreg}
-                  alt="webreg-project"
-                ></img>
+              <div>
+                <div className="card-black">
+                  <img
+                    className="image-container"
+                    src={webreg}
+                    alt="webreg-project"
+                  ></img>
+                </div>
+                <h2>UCSD's WebReg Redesign</h2>
               </div>
-              <h2>UCSD's WebReg Redesign</h2>
-            </div>
-            <div>
-              <div className="card-black">
-                <img
-                  className="image-container"
-                  src={munchies}
-                  alt="munchies-project"
-                ></img>
+              <div>
+                <div className="card-black">
+                  <img
+                    className="image-container"
+                    src={munchies}
+                    alt="munchies-project"
+                  ></img>
+                </div>
+                <h2>Munchie Masters App</h2>
               </div>
-              <h2>Munchie Masters App</h2>
             </div>
           </div>
         </div>
